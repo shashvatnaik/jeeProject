3a7fec56d05b41adf7c75d6134e6e0c9a90c775a
shashvat <shashvatzm@gmail.com>
2021-02-16 11:43:04 +0530

    examly db credentials

--- jeeApp/src/main/java/com/shashvat/jeeProject/util/JDBCUtils.java
+++ jeeApp/src/main/java/com/shashvat/jeeProject/util/JDBCUtils.java
@@ -8,9 +8,9 @@ import java.time.LocalDate;
 
 public class JDBCUtils {
 
-	private static String jdbcURL = "jdbc:mysql://localhost:3306/remindme";
+	private static String jdbcURL = "jdbc:mysql://localhost:3306/remindme?allowPublicKeyRetrieval=true&useSSL=false";
 	private static String jdbcUsername = "root";
-	private static String jdbcPassword = "root";
+	private static String jdbcPassword = "examly";
 
 	public static Connection getConnection() {
 		Connection connection = null;
